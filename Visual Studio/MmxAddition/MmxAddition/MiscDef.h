25e46b01d5102603a52b155548c054e3937961a8
Leonardo Alves da Costa <leonhad@gmail.com>
2019-12-10 17:26:57 -0300

    Started MMX example.

---
+++ Visual Studio/MmxAddition/MmxAddition/MiscDef.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Signed integers
+typedef __int8		Int8;
+typedef __int16		Int16;
+typedef __int32		Int32;
+typedef __int64		Int64;
+
+// Unsigned integers
+typedef unsigned __int8		Uint8;
+typedef unsigned __int16	Uint16;
+typedef unsigned __int32	Uint32;
+typedef unsigned __int64	Uint64;
---
+++ Visual Studio/MmxAddition/MmxAddition/MmxAddition.cpp
@@ -0,0 +1,10 @@
+// MmxAddition.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include "MmxVal.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
