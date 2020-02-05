0781a9c3e4bb0c7349c49a57d40daa7c5cdd3063
Leonardo Alves da Costa <leonhad@gmail.com>
2020-02-05 08:13:21 -0300

    Added a register renaming example.

---
+++ ARM/Keil/Renaming Registers/main.s
@@ -0,0 +1,18 @@
+
+VAL1	RN		R1
+VAL2	RN		R2
+SUM		RN		R3
+
+	AREA rn, CODE, READONLY
+	ENTRY
+	EXPORT __main
+		
+__main
+	MOV		VAL1, #0x25
+	MOV 	VAL2, #0x34
+	ADD		SUM, VAL1, VAL2
+
+Stop
+	B		Stop
+
+	END
